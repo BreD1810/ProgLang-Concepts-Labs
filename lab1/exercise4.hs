61a3e28569f62bd24af6656ec586636105da1c16
BreD1810 <bradleybredgarrod@gmail.com>
2019-02-05 11:49:06 +0000

    Added solution for exercise 4 of lab 1. I used the sample solution as I was inexperienced with IO with files in Haskell. It does throw an error using GHCi, but I think this is normal.

---
+++ lab1/exercise4.hs
@@ -0,0 +1,26 @@
+import System.IO
+import Control.Exception
+import Data.List
+import Data.List.Split
+
+multiZipL :: Eq a => [[a]] -> [[a]]
+multiZipL [] = []
+multiZipL xs = [y | (y:ys) <- xs] : multiZipL [ys | (y:ys) <- xs, ys /= []]
+
+multiFileZipL :: () -> IO ()
+multiFileZipL () = catch (multiFileZipL' ()) filehandler
+
+multiFileZipL' :: () -> IO ()
+multiFileZipL' () = do s <- readFile "lists.csv"
+                       let ls = lines(s)
+                       let sss = map (splitOn ",") ls
+                       let iss = map (map read) sss :: [[Int]]
+                       let mss = multiZipL iss
+                       let oss = map (map show) mss
+                       let css = intercalate "\n" (map (intercalate ",") oss)
+                       writeFile "ziplists.csv" css
+
+filehandler :: IOException -> IO ()
+filehandler e = do let err = show (e :: IOException)
+                   hPutStr stderr ("Warning: Couldn't open input/output file: " ++ err)
+                   return ()
