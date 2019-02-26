6429a7eddf4bc439f2741383a50c5c6c5d10f83f
Bradley Garrod <32489229+BreD1810@users.noreply.github.com>
2019-02-26 15:55:26 +0000

    Wrote a main program to generate a AST value for an input string

---
+++ lab3/Main.hs
@@ -0,0 +1,20 @@
+import LambdaToyTokens
+import LambdaToyGrammar
+import System.IO
+import System.Environment
+import Control.Exception
+
+main :: IO ()
+main = catch main' noParse
+
+main' :: IO ()
+main' = do
+    (fileName : _) <- getArgs
+    code <- readFile fileName
+    let program = parseCalc (alexScanTokens code)
+    putStrLn (show program)
+
+noParse :: ErrorCall -> IO ()
+noParse e = do let err = show e
+               hPutStr stderr err
+               return ()
